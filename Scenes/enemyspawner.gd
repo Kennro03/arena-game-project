529633d746d9c3fbbaaa6f3cd45f7308ccf2a71e
Kennro03 <kennro03@gmail.com>
2025-07-21 20:22:44 +0200

    EnemySpawner

---
+++ Scenes/enemyspawner.gd
@@ -0,0 +1,31 @@
+extends Node
+@export var stickman: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			print("Left mouse button")
+			spawn_stickman(event.position)
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP :
+			print("Scroll wheel up")
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			print("Scroll wheel down")
+
+func spawn_stickman(pos: Vector2):
+	if stickman == null:
+		push_error("No stickman scene assigned!")
+		return
+	
+	var stickman_instance = stickman.instantiate()
+	stickman_instance.position = pos
+	get_parent().add_child(stickman_instance)
