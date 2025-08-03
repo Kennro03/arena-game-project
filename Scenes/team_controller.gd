1b5470a2f23fd2d44c7364a76dd44762e222980d
Kennro <kennro03@gmail.com>
2025-08-03 20:13:20 +0200

    changed team controller to controller type node

--- Scenes/team_controller.gd
+++ Scenes/team_controller.gd
@@ -5,7 +5,7 @@ var flag_instance
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if owner.team != null :
-		print("Adding flag.")
+		print("Stickman has a team, adding flag.")
 		flag_instance = flag.instantiate()
 		flag_instance.modulate = owner.team.team_color
 		add_child(flag_instance)
@@ -13,5 +13,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	flag_instance.position = owner.position + Vector2(0,-80)
 	pass
