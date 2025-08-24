be6e78d295a15f854eeabee73486801ffe954378
Kennro03 <kennro03@gmail.com>
2025-08-24 22:11:13 +0200

    hitbox changes

--- Scenes/Hitboxes/hitbox.gd
+++ Scenes/Hitboxes/hitbox.gd
@@ -1,27 +1,49 @@
 extends Area2D
+signal hitbox_finished(targets: Array)
 
-@export var duration: float = 0.2
-@export var group_filter: String = "Hurtbox"
-
-@export var shape: Shape2D = CapsuleShape2D.new()
-@export var radius: float 
-@export var size: Vector2
-@export var height: float
-@export var point_A: Vector2 
-@export var point_B: Vector2
-
+#Caster and targets
 var caster : Node2D
 var target_list : Array[Node2D]
+#Duration, filter and movement
+@export var duration: float = 1.0
+@export var group_filter: String = "Hurtbox"
+@export var move_over_time: bool = false
+@export var velocity: Vector2 = Vector2.ZERO 
+#Growth (using scale)
+@export var grow_over_time : bool = false
+@export var scale_growth_rate : float = 1.0
+var growth_min_scale : float = 0.25
+var growth_max_scale : float = 4.0
+#Follow target
+@export var follow_target : NodePath
+@export var follow_offset : Vector2 = Vector2.ZERO
+var _target_ref : Node2D = null
+#Shape
+@export var shape: Shape2D = CapsuleShape2D.new()
+var radius: float 
+var size: Vector2
+var height: float
+var point_A: Vector2 
+var point_B: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_shape_and_dimensions()
+	if follow_target != NodePath() :
+		_target_ref = get_node_or_null(follow_target)
+	
+	
 	target_list = get_stickmen_in_area(group_filter)
 	await get_tree().create_timer(duration).timeout
 	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if _target_ref and is_instance_valid(_target_ref) :
+		global_position = _target_ref.global_position + follow_offset
+	
+	if move_over_time : 
+		position += velocity * delta
 	pass
 
 func set_shape_and_dimensions() -> void:
