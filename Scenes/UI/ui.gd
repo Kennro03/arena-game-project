87e939487f556ac64070b53068a21e9baad173df
Kennro03 <kennro03@gmail.com>
2025-11-17 09:58:23 +0100

    Add pause functionality to UI
    Implemented pause toggling via input action in ui.gd, showing or hiding a new PauseLabel. Updated ui.tscn to include PauseLabel with custom styling and a background ColorRect for visual feedback when the game is paused.

--- Scenes/UI/ui.gd
+++ Scenes/UI/ui.gd
@@ -5,9 +5,17 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	instantiate_hotbar_slots()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused == true :
+			get_tree().paused = false
+			$PauseLabel.hide()
+		else :
+			get_tree().paused = true
+			$PauseLabel.show()
 	pass
 
 func instantiate_hotbar_slots():
