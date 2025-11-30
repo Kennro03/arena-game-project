bee99342aafdf45dce8622207de5f3ede7fe7582
Kennro03 <kennro03@gmail.com>
2025-11-30 23:28:04 +0100

    Remove PlayAnimationEffect skill effect
    Deleted PlayAnimationEffect.gd and its UID file from the SkillEffects resources. This effect is no longer needed or has been replaced.

--- ressources/SkillEffects/PlayAnimationEffect.gd
+++
@@ -1,7 +0,0 @@
-extends SkillEffect
-class_name PlayAnimationEffect
-
-@export var animation_name : String
-
-func apply(_caster: Node2D, _context: Dictionary = {}) -> void:
-	_caster.get_node("AnimationPlayer").play(_context.get("animation"))
