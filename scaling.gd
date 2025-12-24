7fdc5efc65db9b7051e7b3a47d656e4f51711390
Kennro <kennro03@gmail.com>
2025-12-24 21:20:45 +0100

    Refactor StatScaling and fix casting animation duplication
    Removed duplicate 'casting' animation resource in stickman_sprite.tscn. Refactored scaling.gd by removing unused ScalingType enum and scaling_stat, and updated StatScaling to use scaling_power instead of scaling_type, with adjusted defaults.

--- scaling.gd
+++ scaling.gd
@@ -1,17 +1,11 @@
 extends Resource
 class_name StatScaling
 
-const scalingPower : float = 0.9
-enum ScalingType {
-	FLAT,
-}
-
 @export var stat : Stats.BuffableStats
-@export var scaling_stat : Stats.BuffableStats
-@export var scaling_value: float
-@export var scaling_type : ScalingType
+@export var scaling_value: float = 1.0
+@export var scaling_power : float = 0.9
 
-func _init(_stat: Stats.BuffableStats = Stats.BuffableStats.MAX_HEALTH, _scaling_value: float = 1.0, _scaling_type: StatScaling.ScalingType = ScalingType.FLAT) -> void:
+func _init(_stat: Stats.BuffableStats = Stats.BuffableStats.ATTUNEMENT, _scaling_value: float = 1.0, _scaling_power: float = 0.9) -> void:
 	stat = _stat
-	scaling_type = _scaling_type
 	scaling_value = _scaling_value
+	scaling_power = _scaling_power
