7f67f5c3e9fa804a3d611e2c287f2f67e93542ee
Kennro <kennro03@gmail.com>
2026-01-10 23:53:39 +0100

    adjusted tooltip to flip if it got cut off by viewport

--- Scenes/UI/tooltip.gd
+++ Scenes/UI/tooltip.gd
@@ -4,14 +4,24 @@ var popup_height : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	global_position = get_viewport().get_mouse_position() + Vector2(12, 12)
+func _process(_delta: float) -> void:
+	global_position = get_viewport().get_mouse_position() + Vector2(12, 12) 
 	adjust_position()
-	pass
+
+func adjust_position() -> void:
+	var vp : Rect2 = get_viewport_rect()
+	var rect : Rect2 = %PanelContainer.get_global_rect()
+	
+	popup_height = %PanelContainer.size.y
+	
+	print("Rect pos:", rect.position, " size:", rect.size, " end:", rect.end)
+	print("VP pos:", vp.position, " size:", vp.size, " end:", vp.end)
+	
+	if not vp.encloses(rect) :
+		%PanelContainer.global_position.y = global_position.y - popup_height
 
 func set_text(t: String = "") -> void:
 	%TextLabel.text = t
@@ -21,14 +31,3 @@ func add_line(t: String = "") -> void:
 
 func clear_text() -> void:
 	%TextLabel.text = ""
-
-
-func adjust_position() -> void:
-	var rect := self.get_global_rect()
-	var vp := self.get_viewport_rect()
-	
-	popup_height = %PanelContainer.size.y
-	if not vp.encloses(rect) :
-		%PanelContainer.position.y = -popup_height
-	else :
-		%PanelContainer.position.y = 0.0
