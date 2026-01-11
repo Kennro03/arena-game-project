96bb747e8a8cfb1f0d9e7db11fc84cf9dd96abb1
Kennro <kennro03@gmail.com>
2026-01-11 14:39:24 +0100

    Refactored tooltip

--- Scenes/UI/tooltip.gd
+++ Scenes/UI/tooltip.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var popup_height : float
+@export var mouse_offset : Vector2 = Vector2(12, 12) 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,20 +8,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	global_position = get_viewport().get_mouse_position() + Vector2(12, 12) 
+	global_position = get_viewport().get_mouse_position() + mouse_offset
 	adjust_position()
 
 func adjust_position() -> void:
 	var vp : Rect2 = get_viewport_rect()
 	var rect : Rect2 = %PanelContainer.get_global_rect()
 	
-	popup_height = %PanelContainer.size.y
+	#print("Rect pos:", rect.position, " size:", rect.size, " end:", rect.end)
+	#print("VP pos:", vp.position, " size:", vp.size, " end:", vp.end)
 	
-	print("Rect pos:", rect.position, " size:", rect.size, " end:", rect.end)
-	print("VP pos:", vp.position, " size:", vp.size, " end:", vp.end)
-	
-	if not vp.encloses(rect) :
-		%PanelContainer.global_position.y = global_position.y - popup_height
+	if rect.end.y > vp.end.y :
+		%PanelContainer.global_position.y -= %PanelContainer.size.y + mouse_offset.y
+	if rect.end.x > vp.end.x :
+		%PanelContainer.global_position.x -= %PanelContainer.size.x + mouse_offset.x
 
 func set_text(t: String = "") -> void:
 	%TextLabel.text = t
