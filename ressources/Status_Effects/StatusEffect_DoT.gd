fa8a8184f0dd5a1d3a46a7a4b65db0e0a969279c
Kennro03 <kennro03@gmail.com>
2026-01-19 09:40:04 +0100

    Tweaks to DoT print

--- ressources/Status_Effects/StatusEffect_DoT.gd
+++ ressources/Status_Effects/StatusEffect_DoT.gd
@@ -8,10 +8,10 @@ var total_damage : float = 0.0
 func on_tick(_target, _effect):
 	var dmg : float = stacks * damage_per_stack
 	if _target.has_method("take_damage"):
-		#print("Burning " + str(_target.display_name) + "for " + str(dmg) + " damage" )
+		#print("Dealing DOT " + str(_target.display_name) + "for " + str(dmg) + " damage" )
 		_target.take_damage(dmg)
 		total_damage += dmg
 
 func on_expire(_target, _effect):
-	#print(str(Status_effect_name) + " DoT status effect ended, dealt a total of " + str(total_damage) + " damage")
+	print(str(Status_effect_name) + " DoT status effect ended, dealt a total of " + str(total_damage) + " damage")
 	pass
