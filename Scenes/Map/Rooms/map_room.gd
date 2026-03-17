e95e10b9e3240b8fe9495b3d1eccdaa997ae82ea
Kennro03 <kennro03@gmail.com>
2026-03-17 21:45:44 +0100

    Add MapRoom Area2D scene and script
    Introduce a new MapRoom Area2D with MapRoom.gd to represent map rooms. The script defines a selected signal, ICONS mapping, onready references to visuals/animation, and properties for room and availability with setters that update visuals, play animations, and handle selection. The scene file is updated to attach the script, add an animation method track that triggers selection, and connect input_event to the script handler for click interactions.

---
+++ Scenes/Map/Rooms/map_room.gd
@@ -0,0 +1,55 @@
+extends Area2D
+class_name MapRoom
+
+signal selected(room: Room)
+
+const ICONS := {
+	Room.Type.NOT_ASSIGNED : [null,Vector2.ONE],
+	Room.Type.BATTLE : [null,Vector2.ONE],
+	Room.Type.AMBUSH : [null,Vector2.ONE],
+	Room.Type.SHOP : [null,Vector2.ONE],
+	Room.Type.TREASURE : [null,Vector2.ONE],
+	Room.Type.EVENT : [null,Vector2.ONE],
+	Room.Type.CAMP : [null,Vector2.ONE],
+	Room.Type.BOSS : [null,Vector2.ONE],
+}
+
+@onready var sprite_2d : Sprite2D = $Visuals/Sprite2D
+@onready var line_2d : Line2D = $Visuals/Line2D
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+var available := false : set = _set_available
+var room : Room : set = set_room
+
+func _set_available(new_value : bool)->void:
+	available = new_value
+	
+	if available :
+		animation_player.play("highlight")
+	elif not room.selected :
+		animation_player.play("RESET")
+
+func set_room(new_data: Room)-> void:
+	room = new_data
+	position = room.position
+	line_2d.rotation = randi_range(0,360)
+	sprite_2d.texture = ICONS[room.type][0]
+	sprite_2d.scale = ICONS[room.type][1]
+
+func show_selected() -> void :
+	line_2d.modulate = Color.WHITE
+
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if not available or not event.is_action_pressed("left_mouse"):
+		return
+	
+	room.selected = true
+	animation_player.play("select")
+	
+	pass # Replace with function body.
+
+
+func _on_map_room_selected()->void:
+	selected.emit(room)
