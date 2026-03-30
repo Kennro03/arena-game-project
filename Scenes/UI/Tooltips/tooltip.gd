62611119c01581846461bbce925c9eda76ed673d
Kennro03 <kennro03@gmail.com>
2026-03-31 01:06:54 +0200

    Refactor tooltip UI into Tooltips folder
    Move and refactor tooltip UI into Scenes/UI/Tooltips: add a reusable Tooltip Control (tooltip.gd, tooltip.tscn) with improved positioning, mouse offset, freeze delay, and RichTextLabel/BBCode support. Add ItemToolTip subclass (item_tooltip.gd and item_tooltip.tscn) that formats item name, type and description with rarity colors. Remove the old Scenes/UI/tooltip.* files and update references (stickman_slot.tscn) to point to the new tooltip scene. Includes UID updates and a test preload in ItemToolTip for local testing.

---
+++ Scenes/UI/Tooltips/tooltip.gd
@@ -0,0 +1,49 @@
+extends Control
+class_name Tooltip
+
+const MOUSE_OFFSET : Vector2 = Vector2(8, 8) 
+
+@onready var text_label: RichTextLabel = %TextLabel
+@onready var tooltip_container: Control = %MarginContainer
+
+@export var freeze_tooltip_delay : float = 15.75
+
+var is_frozen : bool = false
+var hovered : bool = true
+
+func _ready() -> void:
+	await get_tree().create_timer(freeze_tooltip_delay).timeout
+	is_frozen = true
+
+func _process(_delta: float) -> void:
+	if not is_frozen :
+		global_position = get_viewport().get_mouse_position() + MOUSE_OFFSET
+		adjust_position()
+
+func adjust_position() -> void:
+	var vp : Rect2 = get_viewport_rect()
+	var rect : Rect2 = tooltip_container.get_global_rect()
+	
+	if rect.end.y > vp.end.y :
+		tooltip_container.global_position.y -= tooltip_container.size.y + MOUSE_OFFSET.y
+	if rect.end.x > vp.end.x :
+		tooltip_container.global_position.x -= tooltip_container.size.x + MOUSE_OFFSET.x
+	if rect.position.y < vp.position.y :
+		tooltip_container.global_position.y += tooltip_container.size.y + MOUSE_OFFSET.y
+	if rect.position.x < vp.position.x :
+		tooltip_container.global_position.x += tooltip_container.size.x + MOUSE_OFFSET.x
+
+func set_text(t: String = "") -> void:
+	text_label.text = t
+
+func add_line(t: String = "") -> void:
+	text_label.text += "\n"+t
+
+func clear_text() -> void:
+	text_label.text = ""
+
+func _on_margin_container_mouse_entered() -> void:
+	hovered = true
+
+func _on_margin_container_mouse_exited() -> void:
+	hovered = false
---
+++ item_tooltip.gd
@@ -0,0 +1,44 @@
+extends Tooltip
+class_name ItemToolTip
+
+var test_dagger := preload("res://ressources/Weapons/testdagger.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super._ready()
+	text_label.text = ""
+	setup(test_dagger)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	super._process(delta)
+
+func setup(item: Item) -> void:
+	add_item_name(item)
+	text_label.newline()
+	add_item_type(item)
+	text_label.newline()
+	add_item_description(item)
+	pass
+
+func add_item_name(item: Item) -> void:
+	var rarity_color := {
+		Item.Rarity.COMMON: "white",
+		Item.Rarity.UNCOMMON: "green",
+		Item.Rarity.RARE: "cyan",
+		Item.Rarity.EPIC: "purple",
+		Item.Rarity.LEGENDARY: "red",
+		Item.Rarity.CURSED: "dark_gray"
+	}
+	var color: String = rarity_color[item.rarity]
+	text_label.append_text("[center][font_size=10][color=%s]%s[/color][/font_size][/center]" % [color, item.item_name])
+
+func add_item_type(item: Item) -> void :
+	var type_name: String = item.ItemType.keys()[item.item_type].capitalize()
+	var line: String = "[i][color=light_gray]%s[/color][/i]" % [type_name]
+	text_label.append_text(line)
+
+func add_item_description(item: Item) -> void :
+	var item_description: String = item.description
+	var line: String = "%s" % [item_description]
+	text_label.append_text(line)
