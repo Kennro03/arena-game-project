9612d4744a9251e95ee1bfab1095130fb4fcbedc
Kennro03 <kennro03@gmail.com>
2026-04-02 16:29:37 +0200

    Add Armor and Accessory item scripts
    Introduce new Item subclasses for equipment: Accessory and Armor. Both define exported statChanges and passives arrays, an owner property, setup_stats that checks for an owner, and apply/remove_owner_buffs to add or remove stat buffs. Armor also adds an ArmorCategory enum and exported armorCategory. Add corresponding .gd.uid resource identifier files.

---
+++ ressources/Items/Armor.gd
@@ -0,0 +1,31 @@
+extends Item
+class_name Armor
+
+## Armor only has stat boosts and passives
+
+enum ArmorCategoryEnum { LIGHT, MEDIUM, HEAVY }
+
+@export_group("Armor data")
+@export var armorCategory : ArmorCategoryEnum
+
+@export_subgroup("Passives")
+@export var statChanges : Array[StatBuff] = [] #Array of stat buffs applied to holder
+@export var passives : Array = [] #Array of PASSIVES that are yet to be implemented
+
+var owner: Node2D
+
+func _init() -> void:
+	item_type = Item.ItemType.ARMOR
+
+func setup_stats() -> void :
+	if owner == null:
+		printerr("No owner!")
+		return
+
+func apply_owner_buffs(stats: Stats):
+	for buff in statChanges:
+		stats.add_buff(buff)
+
+func remove_owner_buffs(stats: Stats):
+	for buff in statChanges:
+		stats.remove_buff(buff)
